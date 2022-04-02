6a34bb1ada02d91c5b5a6a18404db66942e9fbe5
Bruno Arthur <brunoarthur93@gmail.com>
2022-04-02 04:24:12 -0300

    upando a landing page da academia

---
+++ Treino de React/index.js
@@ -0,0 +1,10 @@
+function CapsLock(props) {
+    const textoInserido = props.texto;
+    const textoEmCapsLopck = textoInserido.toUppercase();
+    return <div>{textoEmCapsLopck}</div>
+}
+
+function Pagina() {
+    return <div>Massinha</div>
+}
+
