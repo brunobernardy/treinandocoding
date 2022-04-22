18c40495ea18da7071c84b5eb90c4029d60caf2b
Bruno Arthur <brunoarthur93@gmail.com>
2022-04-22 01:38:46 -0300

    add primeiros arquivos da calculadora de hora$ de trabalho

---
+++ quantoValeHoraTrabalho/js/Quantocusta.js
@@ -0,0 +1,46 @@
+import Validate from './Validate';
+
+export default class Quantocusta {
+
+    constructor() {
+        this.inputSalary = document.getElementById('salario');
+        this.inputDays = document.getElementById('dias');
+        this.inputHours = document.getElementById('horas');
+
+        this.total = 0;
+        this.resultContent = document.getElementById('resultado');
+    }
+
+    // Total value from all data inputed
+    result(){
+        this.total = (parseFloat(this.inputSalary.value) / this.inputDays.value) / this.inputHours.value;
+    }
+
+    showResults(){
+        this.resultContent.classList.remove('hide');
+        document.querySelector('.calculoFinal').innerHTML = new Intl.NumberFormat('pt-BR',{
+            style: 'currency',
+            currency: 'BRL'
+        }).format(this.total);
+    }
+
+    // Event handle in app
+    eventsHandle(){
+        let submitButton = document.querySelector('.js-submit');
+
+        submitButton.onclick = (e) => {
+            e.preventDefault();
+
+            if(
+                Validate(this.inputSalary, this.inputDays, this.inputHours)
+            ) {
+                this.result();
+                this.showResults(this.total);
+            }
+        }
+    }
+    
+    init() {
+        this.eventsHandle();
+    }
+}
---
+++ quantoValeHoraTrabalho/js/main.js
@@ -0,0 +1,5 @@
+import Quantocusta from "./Quantocusta";;
+import '../scss/global.scss';
+
+const quantocusta = new Quantocusta();
+quantocusta.custa.init();
